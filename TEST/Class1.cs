38eb6fa85d3e066669f5ae99435b2faf50eb2040
ammarkhan91 <muhammadammarkhan@hotmail.com>
2015-08-10 22:57:29 +0500

    add 3rd prop

--- TEST/Class1.cs
+++ TEST/Class1.cs
@@ -7,7 +7,8 @@ namespace TEST
 {
     public class Class1
     {
-        public int TesId { get; set; }
-        public string NewTestId { get; set; }
+        public string Prop1 { get; set; }
+        public string Prop2 { get; set; }
+        public string Prop3 { get; set; }
     }
 }
